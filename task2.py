a610992bc33e36af2fdfbb2038d48022dbfd9d11
Nandan Pansuriya <pansuriyanandan01@gmail.com>
2025-06-21 17:43:49 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,9 @@
+list1 = list(range(1,11))
+
+list2 = list1[:5]
+
+list3 = list2[::-1]
+
+print(f"Original list: {list1}")
+print(f"Extracted first five elements: {list2}")
+print(f"Reversed extracted elements : {list3}")
